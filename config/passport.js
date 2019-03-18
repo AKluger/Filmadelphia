a10b38cefa321b3ce58831d9549d1657accc143e
kmop <42947972+AKluger@users.noreply.github.com>
2019-03-17 22:44:03 -0400

    resources page with links

--- config/passport.js
+++ config/passport.js
@@ -5,6 +5,7 @@ var db = require('../models')
 
 // Telling passport we want to use a Local Strategy. In other words, we want login with a username/email and password
 passport.use(new LocalStrategy(
+  
   // Our user will sign in using an email, rather than a "username"
   {
     usernameField: 'email'
@@ -18,14 +19,13 @@ passport.use(new LocalStrategy(
     }).then(function (dbUser) {
       // If there's no user with the given email
       if (!dbUser) {
-        return done(null, false, {
-          message: 'Incorrect email.'
-        })
+        return done(null, false, { message: 'email not found' }
+        )
       }
       // If there is a user with the given email, but the password the user gives us is incorrect
       else if (!dbUser.validPassword(password)) {
         return done(null, false, {
-          message: 'Incorrect password.'
+          message: 'incorrect password'
         })
       }
       // If none of the above, return the user
